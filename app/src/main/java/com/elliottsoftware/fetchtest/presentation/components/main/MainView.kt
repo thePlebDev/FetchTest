2eb1d2bd508f1806beaf28c36fbdde4de9e8a83d
Tristan Elliott <>
2022-10-29 00:47:42 -0300

    improving the UI by adding a pagger

--- app/src/main/java/com/elliottsoftware/fetchtest/presentation/components/main/MainView.kt
+++ app/src/main/java/com/elliottsoftware/fetchtest/presentation/components/main/MainView.kt
@@ -1,8 +1,7 @@
 package com.elliottsoftware.fetchtest.presentation.components.main
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,19 +14,114 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.elliottsoftware.fetchtest.R
 import com.elliottsoftware.fetchtest.presentation.components.list.LoadingAnimation
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.google.accompanist.pager.*
 
+@OptIn(ExperimentalPagerApi::class, ExperimentalAnimationApi::class)
 @Composable
 fun MainView(onNavigate:(Int) -> Unit){
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("Reasons to hire Tristan Elliott",fontSize = 22.sp,
-            fontWeight = FontWeight.Bold,textAlign = TextAlign.Center,
-            modifier = Modifier.padding(start = 0.dp,16.dp,0.dp,0.dp)
+    val pages = listOf(
+        BoardingPage.First,
+        BoardingPage.Second
+    )
+    val pagerState = rememberPagerState()
+
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        // Display 10 items
+        HorizontalPager(
+            modifier = Modifier.weight(8f),
+            count = 2,
+            state = pagerState,
+            verticalAlignment = Alignment.Top
+        ) { position ->
+            // Our page content
+            PagerScreen(pages[position])
+
+        }
+        HorizontalPagerIndicator(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .weight(1f),
+            pagerState = pagerState,
         )
-        Text("1) Tristan is a great communicator ",fontSize = 18.sp, fontStyle = FontStyle.Italic,
-            textAlign = TextAlign.Center,)
-        NavButton(onNavigate)
+        FinishButton(
+            modifier = Modifier.weight(1f),
+            pagerState = pagerState,
+            onNavigate = onNavigate
+        )
     }
 
+}
+@Composable
+fun PagerScreen(boardingPage: BoardingPage) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .fillMaxHeight(0.7f),
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "Pager Image"
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth(),
+            text = boardingPage.title,
+            fontSize = MaterialTheme.typography.h4.fontSize,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 40.dp)
+                .padding(top = 20.dp),
+            text = boardingPage.description,
+            fontSize = MaterialTheme.typography.subtitle1.fontSize,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@ExperimentalAnimationApi
+@ExperimentalPagerApi
+@Composable
+fun FinishButton(
+    modifier: Modifier,
+    pagerState: PagerState,
+    onNavigate:(Int) -> Unit
+) {
+    Row(
+        modifier = modifier
+            .padding(horizontal = 40.dp),
+        verticalAlignment = Alignment.Top,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        AnimatedVisibility(
+            modifier = Modifier.fillMaxWidth(),
+            visible = pagerState.currentPage == 1
+        ) {
+            Button(
+                onClick = { onNavigate(R.id.action_mainFragment_to_listFragment) },
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = "Get Data")
+            }
+        }
+    }
 }
 
 @Composable
